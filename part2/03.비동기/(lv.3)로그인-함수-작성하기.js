1e2aeb1c8920f4ac56ab44f51089759ae8af4581
siheon <rlatlgjs357@gmail.com>
2025-04-09 10:25:29 +0900

    part2-비동기-(lv 3)로그인-함수-작성하기

--- part2/03.비동기/(lv.3)로그인-함수-작성하기.js
+++ part2/03.비동기/(lv.3)로그인-함수-작성하기.js
@@ -13,7 +13,14 @@
 
 import { loginRequest } from "../../part1/10.비동기/(lv.2)로그인-확인-함수";
 
-async function safeLogin(id, pw) {}
+async function safeLogin(id, pw) {
+  try {
+    const result = await loginRequest(id, pw);
+    return result;
+  } catch (error) {
+    return "error";
+  }
+}
 
 // export를 수정하지 마세요.
 export { safeLogin };
